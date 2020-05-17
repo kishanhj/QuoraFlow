bfc86005f4f5ec65980b3eef6f2c3590edd52d1b
kishan <Kash.hj94@gmail.com>
2020-05-17 19:30:25 -0400

    404 issue

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import Form from "./components/Form";
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 import Question from "./components/questiondisplay";
 import EditQuestion from "./components/editQuestion";
 import DeleteQuestion from "./components/deleteQuestion";
@@ -25,20 +25,23 @@ function App() {
         <div className="App">
           <DashBoard></DashBoard>
           <div className="App-body">
-            <Route exact path="/" component={LandingPage} />
-            <Route exact path="/signup" component={SignUp} />
-            <Route exact path="/signin" component={SignIn} />
-            <PrivateRoute exact path="/answer" ><LandingPage answerPage={true} /></PrivateRoute>
-            <PrivateRoute exact path="/myQuestions" ><LandingPage myQuestions={true} /></PrivateRoute>
-            <PrivateRoute exact path="/questions" exact component={Form} />
-            <PrivateRoute exact path="/questions/display/:id" exact component={Question} />
-            <PrivateRoute exact path="/questions/edit/:id" exact component={EditQuestion} />
-            <PrivateRoute exact path="/questions/delete/:id" exact component={DeleteQuestion} />
-            <Route exact path="/notfound" component={PagenotFound} status={404} />
-            <PrivateRoute exact path="/search" component={Search} />
-            <Route exact path="/username" component={UserName} />
-            <PrivateRoute exact path="/tag/:id" component={TagPage} />
-            <Route exact path="/deleted" component={Questiondeleted}/>
+            <Switch>
+              <Route exact path="/" component={LandingPage} />
+              <Route exact path="/signup" component={SignUp} />
+              <Route exact path="/signin" component={SignIn} />
+              <PrivateRoute exact path="/answer" ><LandingPage answerPage={true} /></PrivateRoute>
+              <PrivateRoute exact path="/myQuestions" ><LandingPage myQuestions={true} /></PrivateRoute>
+              <PrivateRoute exact path="/questions" exact component={Form} />
+              <PrivateRoute exact path="/questions/display/:id" exact component={Question} />
+              <PrivateRoute exact path="/questions/edit/:id" exact component={EditQuestion} />
+              <PrivateRoute exact path="/questions/delete/:id" exact component={DeleteQuestion} />
+              <Route exact path="/notfound" component={PagenotFound} status={404} />
+              <PrivateRoute exact path="/search" component={Search} />
+              <Route exact path="/username" component={UserName} />
+              <PrivateRoute exact path="/tag/:id" component={TagPage} />
+              <Route exact path="/deleted" component={Questiondeleted}/>
+              <Route component={PagenotFound} status={404}></Route>
+            </Switch>
             {/* <CommentList comments={comments} /> */}
           </div>
         </div>
